a30f55b77287096b254d1537dc0f57692a1479aa
Karma-234 <busari1kamal@gmail.com>
2023-08-06 10:16:10 -0700

    WIP

--- async/async.js
+++ async/async.js
@@ -1,28 +1,84 @@
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
+const getTodosAsync = async()=>{
+    const response = await fetch('https://jsonplaceholder.typicode.com/todo/');
+    if (response.status !== 200) {
+        throw new Error('Unable to fetch data!')
+    }
+    const data = await response.json();
+    
+    return data;
+}
+
+// fetch('https://jsonplaceholder.typicode.com/todos/')
 //       .then(response => {
 //         console.log(response.url);
 //        return response.json();
         
 //       })
 //       .then(json => {
-//         if (json['completed'].toString().toLowerCase().includes('false')) {
-//             console.log('NOT COMPLETED!!!!!!!');   
-//         }
+//        myCallback(json);
 //         console.log(json);
 //         console.log('DONE!!!!!!!');      
-//     });
+//     }).catch(err => {});
+// fetch
 
-    
-    const req = ()=>{
-     const  request =new XMLHttpRequest();
-    request.addEventListener('readystatechange',()=>{
-        if (request.readyState === 4 && request.status === 200) {
-            console.log(request.responseText);
-        } else if (request.readyState === 4 ){
-        alert('Unable to fetch data.');
-        }
-    });
-    request.open('GET','https://jsonplaceholder.typicode.com/todos/');
-    request.send();
+    const todoList = document.querySelector('.todolist');
+    const getTodos = () =>{
+        return new Promise((resolve, reject) =>{
+            const  request = new XMLHttpRequest();
+            request.addEventListener('readystatechange',()=>{
+                if (request.readyState === 4 && request.status === 200) {
+                    const myData = JSON.parse(request.responseText);
+                    resolve(myData);
+                } else if (request.readyState === 4 ){
+                    reject('error getting content!');
+                }
+            });
+            request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+            request.send();
+        });
+  
+  
     }
-       
+    const addTodos = (userId, completed, todo)=>{
+       let  isComleted  = completed ? 'Completed':'Pending' ;
+        const newTodo = document.createElement('li');
+        newTodo.innerHTML = `
+        <h2 class="title"> ${todo}
+        <span class="userId">${userId}</span>
+        </h2>
+        <div class="check">${isComleted}</div>
+        `;
+        const targetTodo =   newTodo.classList.add('todo');
+      todoList.prepend(targetTodo);
+    }
+    
+    const myCallback = ( data)=>{
+        let counter = 0;
+        console.log(data);
+      
+        // data.forEach(e => {
+        //     if (counter < 10) {
+        //         addTodos(e['userId'], e['completed'], e['title']);
+        //     }
+        //     counter++;
+
+        // });
+        
+        
+    }
+
+    // getTodos().then(todos=> {
+    //     // console.log(todos);
+    //     myCallback(todos);
+    // }).catch(err => {
+        
+    //     if (err) {
+    //         console.log('Promise rejected', err);
+    //     }
+    // });
+
+   getTodosAsync().then(todos=> {
+    console.log(todos);
+   }).catch(err => {
+    console.log(err);
+   });
---
+++ async/index.tsx
@@ -0,0 +1,6 @@
+let myNumber: Number  = 7;
+function add(params:Number) {
+    this.myNumber += params;
+    console.log(this.myNumber);
+}
+console.log(8);
