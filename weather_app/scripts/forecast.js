9305acee05a3aca6f534dd47f7f888ad314305e4
Karma-234 <busari1kamal@gmail.com>
2023-08-31 10:52:33 -0700

    WIP: Prototyoe model and class sugar syntax

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -1,1 +1,77 @@
-const key = 'X0nAM2LC6p4xtuVi1vsN2ltnjOB2xvZO';
+const key = 'nyYjDa2TaV8AXnpNGpYfsA6E1kNdxCKq';
+
+
+// Sugar Syntax of Writing class.
+class User1 {
+     name;
+     number
+
+    constructor(userName, number){
+this.name = userName;
+this.number = number;
+    }
+   login(){
+    console.log(`${this.name} just logged in`);
+    return this;
+   }
+   logout(){
+    console.log(`${this.name} just logged out`);
+    return this;
+   }
+}
+
+//Inheritance with modern class declaration.
+class Admin1 extends User1 {
+    constructor(name, number, title){
+        super(name,number);
+        this.title = title;
+    }
+    clearUsers(){
+        console.log(`${this.name} has cleared all users`);
+    }
+}
+
+const getCity = async (city) =>{
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=${key}&q=${city}`
+
+    const resp = await fetch(base + query,{'mode':'no-cors'});
+    const data = await  resp.json();
+    return data;
+
+}
+getCity('manchester');
+
+// Prototype model of writing class
+function User(userName, number){
+    this.userName = userName;
+    this.number = number;
+
+}
+User.prototype.login = function(){
+    console.log(`${this.userName} just logged in`);
+    return this;
+
+}
+User.prototype.logout = function(){
+    console.log(`${this.userName} just logged out`);
+    return this;
+}
+
+// Inheritance with prototype model
+function Admins (userName,number,title){
+    User.call(this,userName,number);
+    this.title = title;
+}
+Admins.prototype = Object.create(User.prototype);
+Admins.prototype.clearUsers = function(){console.log(`${this.title} has cleared all users`);};
+
+
+const firstUser = new User('alidev',44);
+const secondUser = new User1('kamzydev',63);
+const firstAdmin = new Admin1('adminKamzy',32,'Super Admin');
+const secondAdmin = new Admins('adminKamzy',32,'Super Admin');
+firstUser.login().logout();
+secondUser.login().logout();
+firstAdmin.login().logout().clearUsers();
+secondAdmin.login().logout().clearUsers();
