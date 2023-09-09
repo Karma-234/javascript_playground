543089245676d81c0a7c2feb546ee5f1b6a998df
Karma-234 <busari1kamal@gmail.com>
2023-09-09 03:05:33 -0700

    Completed UI responveness to nav selector

---
+++ chat_room/chat.js
@@ -0,0 +1,37 @@
+const roomList = document.querySelector(".room-options");
+const rooms = document.querySelectorAll(".room");
+const messageField = document.querySelector("[name='message']");
+const updateField = document.querySelector("[name='update']");
+const sendButton = document.querySelector("[name='send']");
+const updateButton = document.querySelector("[name='update']");
+const chats = document.querySelectorAll(".chat");
+const selectTab = id=>{
+    rooms.forEach(room=>{
+        room.getAttribute('id') !== id ?room.classList.remove('selected') :null ;
+    });
+    chats.forEach(chat=>{
+        chat.getAttribute('id') !== id ?chat.classList.add('hide') :chat.classList.remove('hide')  ;
+    });
+}
+roomList.addEventListener('mouseout',e=>{
+    
+    if (e.target.tagName==='P' && e.target.classList.contains('hover')) {
+        e.target.classList.remove('hover');
+    }
+});
+roomList.addEventListener('mousemove',e=>{
+    
+    if (e.target.tagName==='P' && !e.target.classList.contains('hover')) {
+        e.target.classList.add('hover');
+    }
+});
+roomList.addEventListener('click',e=>{
+    
+    // console.log(id);
+    if (e.target.tagName==='P' && !e.target.classList.contains('selected')) {
+        const id = e.target.getAttribute('id');
+        e.target.classList.add('selected');
+        selectTab(id);
+        
+    }
+});
